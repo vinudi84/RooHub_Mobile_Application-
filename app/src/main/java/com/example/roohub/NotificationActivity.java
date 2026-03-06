d01ae7a16c41082b6e899759111197108934cd5f
Your Name <you@example.com>
2026-03-06 15:30:17 +0530

    added notification

--- app/src/main/java/com/example/roohub/NotificationActivity.java
+++ app/src/main/java/com/example/roohub/NotificationActivity.java
@@ -1,24 +1,22 @@
 package com.example.roohub;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class NotificationActivity extends AppCompatActivity {
+
+    ImageView btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_notification);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        btnBack = findViewById(R.id.btnBack);
+
+        // Back button wita  (Home ekata yano)
+
+        btnBack.setOnClickListener(v -> finish());
     }
 }
