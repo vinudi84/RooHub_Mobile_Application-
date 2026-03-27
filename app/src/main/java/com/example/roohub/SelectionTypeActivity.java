8ae01026882b13dc5bf915ce9f59c33f6615a147
Your Name <you@example.com>
2026-03-27 14:14:00 +0530

    Added selection type logic and activity

--- app/src/main/java/com/example/roohub/SelectionTypeActivity.java
+++ app/src/main/java/com/example/roohub/SelectionTypeActivity.java
@@ -2,41 +2,49 @@ package com.example.roohub;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class SelectionTypeActivity extends AppCompatActivity {
 
-
-    private Button btnTeacher, btnArtist;
+    Button btnTeacher, btnArtist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_selection_type);
 
+        View mainView = findViewById(R.id.main);
+        if (mainView != null) {
+            ViewCompat.setOnApplyWindowInsetsListener(mainView, (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+        }
 
+        // Buttons
         btnTeacher = findViewById(R.id.btnTeacher);
         btnArtist = findViewById(R.id.btnArtist);
 
-
+        // Teacher Button Click Logic
         if (btnTeacher != null) {
             btnTeacher.setOnClickListener(v -> {
-
-
-                Intent intent = new Intent(SelectionTypeActivity.this, RegisterActivity.class);
+                Intent intent = new Intent(SelectionTypeActivity.this, teacherSignUp.class);
                 startActivity(intent);
             });
         }
 
-
+        // Artist Button Click Logic
         if (btnArtist != null) {
             btnArtist.setOnClickListener(v -> {
-                // UploadActivity ව
                 Intent intent = new Intent(SelectionTypeActivity.this, UploadActivity.class);
                 startActivity(intent);
             });
